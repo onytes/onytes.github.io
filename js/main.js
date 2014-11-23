fb31e5b4b619d1d70225b3a9e730ef0b3c685b61
Pablo Vieytes <pablo@onytes.com>
2014-11-23 01:25:54 +0100

    working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+var speed = 1000;
 
+var startup  = $("#startup").offset();
+
+
+function scrollTo(id){
+	if (id == 'startup')
+	{
+	$("html, body").animate({ scrollTop: startup.top} , speed) ;	
+	};
+};
